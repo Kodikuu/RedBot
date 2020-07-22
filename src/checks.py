2daa2de30a7db89aaf76f9e2bda325478f2816c9
Kodikuu <callum.james.watson@gmail.com>
2020-07-22 09:56:14 +0100

    Some checks

---
+++ src/checks.py
@@ -0,0 +1,22 @@
+def category_robotics(ctx):
+    return ctx.channel.category.name.lower() == "robotics facility"
+
+
+def chan(ctx, name):
+    return ctx.channel.name == name
+
+
+def chan_staff(ctx):
+    return chan(ctx, "staff")
+
+
+def chan_commands(ctx):
+    return chan(ctx, "bot_commands")
+
+
+def chan_assignment(ctx):
+    return chan(ctx, "role_assignment")
+
+
+def chan_log(ctx):
+    return chan(ctx, "redbot_log")
